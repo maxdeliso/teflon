599ceb7a49f6579c10f216bfbf58045ae9709176
jmd5473 <jmd5473@PATTEEW116-53.win.psu.edu>
2012-09-02 22:38:26 -0400

    bunch of comments

--- Teflon/src/teflon/Teflon.java
+++ Teflon/src/teflon/Teflon.java
@@ -36,6 +36,11 @@ import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 class Teflon {
+   /*
+    * These are constants which are global to the entire application. In the
+    * future they should probably be moved into a preference file so that they
+    * can be configured by the user.
+    */
    public static final int TEFLON_PORT = 1337;
    public static final byte[] TEFLON_RECV_ADDRESS = new byte[] { 0, 0, 0, 0 };
    public static final byte[] TEFLON_SEND_ADDRESS = new byte[] { (byte) 0xff, (byte) 0xff,
@@ -44,10 +49,22 @@ class Teflon {
    public static final int INPUT_BUFFER_LEN = 1024;
    public static final Charset UTF8_CHARSET = Charset.forName("UTF-8");
 
+   /*
+    * These are are application global variables. A reference to each thread and
+    * a boolean maintaining whether or not the application is alive. These
+    * references are maintained here so that each handler can easily and
+    * elegantly obtain a reference to any other handlers, as well as being able
+    * to signal application exit uniformly.
+    */
    private TeflonRemoteHandler remoteHandler;
    private TeflonLocalHandler localHandler;
    private boolean alive;
 
+   /*
+    * This is an application global helper routine which is used to signal a
+    * bug. I pass all unanticipated exceptions here which makes fixing bugs a
+    * lot easier.
+    */
    private static void reportException(Exception ex) {
       System.err.println("ERROR: " + Thread.currentThread() + " : " + ex);
       ex.printStackTrace();
@@ -55,40 +72,93 @@ class Teflon {
       System.exit(1);
    }
 
+   /*
+    * This is an application global helper routine which is used to report an
+    * arbitrary string along with the thread which emanated it. It's very
+    * convenient to have all debugging output statements pass through a
+    * centralized gate so that they might be easily disabled in a release build.
+    */
    private static void debugMessage(String message) {
       System.out.println("DEBUG: " + Thread.currentThread() + " : " + message);
    }
 
+   /*
+    * This is the application entry point. Note that the application's command
+    * line arguments are currently ignored, and all that it does is instantiate
+    * a Teflon object. The Teflon object, which is the top level class for the
+    * application, contains all application logic. It is also useful to note
+    * that every single line of code that executes in this program can be traced
+    * back to this root invocation of the constructor of Teflon.
+    */
    public static void main(String args[]) {
       new Teflon();
    }
 
+   /*
+    * This is a utility function to convert a string of bytes into a java
+    * string. This is necessary because strings need to be converted to and from
+    * a byte representation in order to pass over the network. The java virtual
+    * machine takes care of abstractions at the transport layer, but you still
+    * need to care about the character set.
+    */
    public static String decodeUTF8(byte[] bytes) {
       return new String(bytes, UTF8_CHARSET);
    }
 
+   /*
+    * This function is a simple inverse of decodeUTF8.
+    */
    public static byte[] encodeUTF8(String string) {
       return string.getBytes(UTF8_CHARSET);
    }
 
+   /*
+    * This function returns a reference to the remote handler, which is useful
+    * when another child of Teflon wants to be able to communicate with the
+    * remote handler, for instance the local handler.
+    */
    public TeflonRemoteHandler remote() {
       return remoteHandler;
    }
 
+   /*
+    * This function returns a reference to the local handler, it is useful for
+    * the same reasons the remote() function is useful.
+    */
    public TeflonLocalHandler local() {
       return localHandler;
    }
 
+   /*
+    * This function returns a boolean which the caller interprets to mean that
+    * it should keep executing. Note that this is a synchronized method which
+    * means that all invocations of this method on the same instance of this
+    * class will block each other. It is used by the handler threads to check
+    * that they should continue executing.
+    */
    public synchronized boolean alive() {
       return alive;
    }
 
+   /*
+    * This function sets the alive flag to false, effectively signaling to all
+    * the parts of the application that they should initiate an orderly
+    * shutdown. It is used by the handlers when they wish to signal termination.
+    */
    public synchronized void kill() {
       alive = false;
    }
 
+   /*
+    * This is the constructor of the application class. It creates instances of
+    * the local and remote handlers and passes them an instance of itself, so
+    * that they may easily acquire references to each other, and any other
+    * handlers that may be introduced in the future. Note that the handlers all
+    * implement Runnable and are started in their own threads, which means that
+    * they execute concurrently, or at the same time.
+    */
    public Teflon() {
-      alive = new Boolean(true);
+      alive = true;
       remoteHandler = new TeflonRemoteHandler(this);
       localHandler = new TeflonLocalHandler(this);
       Thread remoteHandlerThread = new Thread(remoteHandler);
@@ -107,6 +177,10 @@ class Teflon {
       debugMessage("main thread exiting");
    }
 
+   /*
+    * The message class describes a message. It needs to be extended
+    * significantly.
+    */
    private class Message {
       public String id, body;
 
@@ -121,10 +195,22 @@ class Teflon {
       }
    }
 
+   /*
+    * The CommDestiny interface signifies that a class can serve as a
+    * destination for communications. Both the local and remote handlers
+    * implement this class, and use the queueMessage(...) method to send each
+    * other data.
+    */
    private interface CommDestiny {
       public void queueMessage(Message msg);
    }
 
+   /*
+    * The TeflonLocalHandler is responsible for interacting with the user on the
+    * local machine. Its duties include displaying the user interface, handling
+    * all user input events, and handling any messages it receives from other
+    * threads, but usually messages from the remote handler.
+    */
    @SuppressWarnings("serial")
    private class TeflonLocalHandler extends JFrame implements Runnable, CommDestiny {
       private static final int TEFLON_WIDTH = 512;
@@ -300,6 +386,13 @@ class Teflon {
       }
    }
 
+   /*
+    * The TeflonRemoteHandler is responsible for sending and receiving messages
+    * from other computers. Note that it implements CommDestiny and therefore
+    * represents an entity which is capable of receiving messages for delivery.
+    * Also note that it implements Runnable and can therefore run in its own
+    * thread. It is mostly responsible for handling its UDP socket.
+    */
    private class TeflonRemoteHandler implements Runnable, CommDestiny {
       private InetAddress listeningAddress;
       private DatagramSocket udpSocket;
