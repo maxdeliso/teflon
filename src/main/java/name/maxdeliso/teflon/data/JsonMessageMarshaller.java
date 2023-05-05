54f5a03b2b2ce67bbd76a7b5eb232b2768d2d954
Max DeLiso <me@maxdeliso.name>
2023-05-04 21:42:09 -0400

    remove unnecessary stream code

--- src/main/java/name/maxdeliso/teflon/data/JsonMessageMarshaller.java
+++ src/main/java/name/maxdeliso/teflon/data/JsonMessageMarshaller.java
@@ -2,9 +2,6 @@ package name.maxdeliso.teflon.data;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
@@ -22,13 +19,10 @@ public class JsonMessageMarshaller implements MessageMarshaller {
 
   @Override
   public Optional<Message> bufferToMessage(final ByteBuffer bb) {
-    var parseBuffer = new byte[bb.limit()];
-    bb.get(parseBuffer);
-
-    try (final var stream = new ByteArrayInputStream(parseBuffer);
-         final var reader = new InputStreamReader(stream, MESSAGE_CHARSET)) {
-      return Optional.ofNullable(gson.fromJson(reader, Message.class));
-    } catch (IOException | JsonSyntaxException exc) {
+    try {
+      var buffer = MESSAGE_CHARSET.decode(bb).toString();
+      return Optional.ofNullable(gson.fromJson(buffer, Message.class));
+    } catch (JsonSyntaxException exc) {
       return Optional.empty();
     }
   }
